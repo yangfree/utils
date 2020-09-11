744fc8c40597bd1a86067da7eafa278b53285d33
yangfree <hbyangjie@gmail.com>
2020-09-11 16:41:12 +0800

    add 2020-09-11 添加获取通用查询字符串参数方法

--- src/utils.js
+++ src/utils.js
@@ -60,6 +60,35 @@ function isJavaScript() {
     return "Browser";
 }
 
+/**
+ * Soleve the problem: 获取通用查询字符串参数
+ *  @param: null
+ *  return Object
+ * 
+ */
+function getQueryStringArgs() {
+    var qs = location.search.length > 0 ? location.search.substring(1) : "",
+      args = {},
+      items = qs.length ? qs.split("&") : [],
+      item = null,
+      name = null,
+      value = null,
+      i = 0,
+      len = items.length;
+  
+    for (i = 0; i < len; i++) {
+      item = items[i].split("=");
+      name = decodeURIComponent(items[0]);
+      value = decodeURIComponent(items[1]);
+  
+      if (name.length) {
+        args[name] = value;
+      }
+    }
+  
+    return args;
+  }
+
 
 
 
