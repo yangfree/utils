1003424c8f3c1170ba9c2dfa394578f2b45111c9
yangfree <hbyangjie@gmail.com>
2020-09-22 11:04:06 +0800

    UPDATE 2020-09-22 击鼓传花代码完善

--- src/data_structure/2.队列结构.js
+++ src/data_structure/2.队列结构.js
@@ -40,5 +40,30 @@ class Queue {
 }
 
 /**
- * 击鼓传花规则和实现
+ * 击鼓传花实现:
+ *   @param: 人数列表
+ *   @param: 要数的数字
+ *   @return 返回最后剩余的人 
  */
+function passGame(nameList, number) {
+    let queue = new Queue()
+
+    // 1. 依次进入队列
+    for (let i = 0; i < nameList.length; i++) {
+        queue.enqueue(nameList[i])
+    }
+
+    // 2. 开始数数字,当队列中数字大于1的时候
+    while (queue.size() > 1) {
+        for (let i = 0; i < number - 1; i++) {
+            // 3. 不是这个数字的人从队列前端删除放入到队列末尾
+            queue.enqueue(queue.dequeue())
+        }
+
+        // 4. 数到number的人直接从队列中删除掉
+        queue.dequeue()
+    }
+
+    // 5. 返回所在原来列表中的下标
+    return queue.front()
+}
