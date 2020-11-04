15738f21e39fd3799e0921f9489e1a5010fac5bb
yangfree <hbyangjie@gmail.com>
2020-11-04 17:29:17 +0800

    UPDATE 2020-11-04 更新utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 /**
  * Solve the problem: 格式化时间 
  * @param {String: date}
- * return 2019-10-28 16:44:30 (格式化后的时间字符串)
+ * @return 2019-10-28 16:44:30 (格式化后的时间字符串)
  * 
  */
 function formatDate(date) {
@@ -32,7 +32,7 @@ function formatDate(date) {
 /**
  * Solve the problem: 对象深度克隆
  * @param: [Object source]
- * return newObject
+ * @return newObject
  *
  */
 function objDeepCopy(source) {
@@ -47,9 +47,9 @@ function objDeepCopy(source) {
 };
 
 /**
- * Soleve the problem: 判断js执行环境
+ * Solve the problem: 判断js执行环境
  * @param: window
- * return String
+ * @return String
  *
  * */
 function isJavaScript() {
@@ -61,37 +61,90 @@ function isJavaScript() {
 }
 
 /**
- * Soleve the problem: 获取通用查询字符串参数
+ * Solve the problem: 获取通用查询字符串参数
  *  @param: null
- *  return Object
+ *  @return Object
  * 
  */
 function getQueryStringArgs() {
     var qs = location.search.length > 0 ? location.search.substring(1) : "",
-      args = {},
-      items = qs.length ? qs.split("&") : [],
-      item = null,
-      name = null,
-      value = null,
-      i = 0,
-      len = items.length;
-  
+        args = {},
+        items = qs.length ? qs.split("&") : [],
+        item = null,
+        name = null,
+        value = null,
+        i = 0,
+        len = items.length;
+
     for (i = 0; i < len; i++) {
-      item = items[i].split("=");
-      name = decodeURIComponent(items[0]);
-      value = decodeURIComponent(items[1]);
-  
-      if (name.length) {
-        args[name] = value;
-      }
+        item = items[i].split("=");
+        name = decodeURIComponent(items[0]);
+        value = decodeURIComponent(items[1]);
+
+        if (name.length) {
+            args[name] = value;
+        }
     }
-  
+
     return args;
-  }
+}
+
+/**
+ * Solve the problem: 获取随机数字+字母组成的n位验证码
+ * @param {n} Number    参数是位数
+ * @return {tem} String  返回随机字符串
+ */
+function randomString(n) {
+    let str = 'abcdefghijklmnopqrstuvwxyz9876543210';
+    let tmp = '',
+        i = 0,
+        l = str.length;
+    for (i = 0; i < n; i++) {
+        tmp += str.charAt(Math.floor(Math.random() * l));
+    }
+    return tmp;
+}
+
+/**
+ * Solve the problem: 获取随机数字+字母组成的n位验证码
+ * @param {len} Number 参数是位数
+ * @return {str} String 返回随机后的字符串
+ */
+function getRandomStr(len) {
+    var str = "";
+    for (; str.length < len; str += Math.random().toString(36).substr(2));
+    return str.substr(0, len);
+}
 
 
+/**
+ * Solve the problem: 获取字符串中出现次数最多的字母和出现的次数
+ *
+ * @param {str} 传入的字符串
+ * @return {Object} 返回出现最多的字母和出现的次数
+ */
+function getStr(str) {
+    if (typeof str !== 'string') return false
 
+    let obj = {}
+    let arr = str.split('')
 
+    for (let i = 0; i < arr.length; i++) {
+        let char = arr[i]
+        obj[char] = obj[char] || 0
+        obj[char] += 1
+    }
 
+    let maxChar
+    let maxNum = 0
+
+    for (let key in obj) {
+        if (obj[key] > maxNum) {
+            maxChar = key
+            maxNum = obj[key]
+        }
+    }
+    return { maxChar, maxNum }
+}
 
 
